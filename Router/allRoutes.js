4afe4678cba29773764492b67a4f4b98edf9c140
chloe2841 <chloe.gaschet.ighemine@gmail.com>
2024-05-14 13:37:59 +0400

    modification de la page service

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,7 +4,8 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/habitat", "Habitat", "/pages/habitat.html"),
-    new Route("/service", "Service", "/pages/service.html"),];
+    new Route("/service", "Service", "/pages/service.html"),
+    new Route("/contact", "Contact", "/pages/contact.html"),];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Zoo Arcadia";
