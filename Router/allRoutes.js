a1601fec10b684d3d31720615d32940515dce22d
chloe2841 <chloe.gaschet.ighemine@gmail.com>
2024-05-14 14:11:42 +0400

    Ajout page Connexion et Avis

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,7 +5,9 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/habitat", "Habitat", "/pages/habitat.html"),
     new Route("/service", "Service", "/pages/service.html"),
-    new Route("/contact", "Contact", "/pages/contact.html"),];
+    new Route("/contact", "Contact", "/pages/contact.html"),
+    new Route("/connexion", "Connexion", "/pages/connexion.html"),
+    new Route("/avis", "avis", "/pages/avis.html"),];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Zoo Arcadia";
